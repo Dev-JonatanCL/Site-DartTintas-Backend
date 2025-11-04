78bec3aac2b11e256b84e9dfb1fcbe5dd3601307
Jonatan C. Luz <dev.jonatancl@gmail.com>
2025-11-03 21:04:19 -0300

    Change import from Tintas.ts to Tintas.js

--- src/controllers/TintaController.ts
+++ src/controllers/TintaController.ts
@@ -1,5 +1,5 @@
 import type { Request, Response } from 'express';
-import Tinta from '../models/Tintas.ts';
+import Tinta from '../models/Tintas.js';
 
 export default {
     async listar (req: Request, res: Response){
